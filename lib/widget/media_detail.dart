75c932eba7c7e5e95bd9f030eafdf01d085b03f2
Tony <tony.inuma@gmail.com>
2020-06-24 05:18:57 -0500

    [FEAT] Movie image banner with shadow

--- lib/widget/media_detail.dart
+++ lib/widget/media_detail.dart
@@ -22,7 +22,38 @@ class MediaDetail extends StatelessWidget {
             filter: ui.ImageFilter.blur(sigmaX: 5.0,sigmaY: 5.0),
             child: Container(
               color: Colors.black.withOpacity(0.5),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 390.0,
+                      height: 390.0,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          media.getPosterUrl()
+                        )
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 20.0,
+                          offset: Offset(0.0, 10.0)
+                        )
+                      ]
+                    ),
+                  ),
+                ],
+              ),
             ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(20.0),
+            )
           )
         ],
       ),
